ced32246e50618839b8eae807815526315c1a5b8
zora89 <zorawar.purohit@gmail.com>
2023-09-25 02:25:28 +0530

    Nav Menu Updated with word 'Create'

--- rms/settings.py
+++ rms/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [
     ".ngrok-free.app",
 ]
 
-ngrok_url = "https://6964-122-162-147-191.ngrok-free.app"
+ngrok_url = "https://fb39-2405-201-19-40ae-d73-3eda-d94b-c165.ngrok-free.app"
 
 
 AUTH_USER_MODEL = "users.User"
